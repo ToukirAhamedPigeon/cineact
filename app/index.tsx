f903868e4f1a2982a5afb7c28e80b303d16f1239
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-08-29 16:00:43 +0600

    feat: add onboarding and movie details screens, enhance index layout with navigation links

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex flex-col w-screen h-screen items-center justify-center bg-white">
-      <Text className="text-5xl">Welcome</Text>
+      <Text className="text-5xl  text-dark-200">Welcome !!</Text>
+      <Link href="/onboarding" className="mt-10 p-4 bg-blue-500 rounded">
+        <Text className="text-white text-lg">Go to Onboarding</Text>
+      </Link>
+      <Link href={{ pathname: "/movie/[id]", params: { id: "avengers" } }} className="mt-4 p-4 bg-green-500 rounded">
+        <Text className="text-white text-lg">Go to Movie Avengers</Text>
+      </Link>
     </View>
   );
 }
