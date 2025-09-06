c8a56cb2d0fa935d5c605c9c41d62a9b713db41e
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-09-06 19:04:48 +0600

    feat: add search bar component and update app icon
    - Implement SearchBar component with basic styling and search icon
    - Update app configuration to use logo.png as icon across all platforms
    - Integrate SearchBar into home screen layout with background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
+import SearchBar from '@/components/SearchBar'
 
 const index = () => {
   return (
-    <View>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className="absolute w-full z-0"/>
+
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight:"100%", paddingBottom:10}}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+        <View className="flex-1 mt-5">
+          <SearchBar/>
+        </View>
+      </ScrollView>
     </View>
   )
 }
